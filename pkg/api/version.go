5484c02d512b7e6a386a6bff9797a5b249583184
yult123 <33591292+yult123@users.noreply.github.com>
2024-10-02 17:02:08 +0800

    feat(ver): add kubevpn

--- pkg/api/version.go
+++ pkg/api/version.go
@@ -1,7 +1,25 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/wencaiwulue/kubevpn/v2/pkg/daemon"
+	"github.com/wencaiwulue/kubevpn/v2/pkg/daemon/rpc"
+)
 
 func GetVersion(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("v1.0"))
+	ver := getDaemonVersion()
+	w.Write([]byte(ver))
+}
+
+func getDaemonVersion() string {
+	cli := daemon.GetClient(false)
+	if cli != nil {
+		version, err := cli.Version(context.Background(), &rpc.VersionRequest{})
+		if err == nil {
+			return version.Version
+		}
+	}
+	return "unknown"
 }
